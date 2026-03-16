57d6149323b5ced50283d78834f4e5e6ce9e0e1b
Emad588 <emadfaisal588@gmail.com>
2026-03-16 15:29:23 +0530

    fix render database config

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -18,18 +18,8 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-      max: 10,
-      min: 2,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
+  dialect: 'sqlite',
+  storage: path.join(__dirname, '../../data/moci_inventory.sqlite'),
+  logging: false,
+},
 };
